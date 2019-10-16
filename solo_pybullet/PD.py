9b82aba3432776450e6f3dc17af2c5bf104ef086
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-16 18:18:41 +0200

    style
    This commit is only about minor style issues: we try to follow
    [PEP8](https://www.python.org/dev/peps/pep-0008/) thanks to
    [flake8](http://flake8.pycqa.org/en/latest/) and
    [yapf](https://github.com/google/yapf), which means:
    
    - one space after a comma
    - comments start with `# `
    - lines should not be more than 120 characters (actually, pep8 says 80…)
    - indentations are 4 spaces
    - no end-of-line spaces
    - one space before and after each binary operator
    - inline comments are separated from the code by 2 spaces
    - avoid non-breakable spaces

--- solo_pybullet/PD.py
+++ solo_pybullet/PD.py
@@ -1,14 +1,15 @@
-# coding: utf8
+# coding: utf8
 
 # Load modules
 import numpy as np
 
-def PD(qa_ref, qa_dot_ref, qa, qa_dot, dt, Kp=1, Kd=1, torque_sat=100, torques_ref=np.zeros((8,1))):
+
+def PD(qa_ref, qa_dot_ref, qa, qa_dot, dt, Kp=1, Kd=1, torque_sat=100, torques_ref=np.zeros((8, 1))):
 
     # Output torques
     torques = Kp * (qa_ref - qa) + Kd * (qa_dot_ref - qa_dot) + torques_ref
-    
+
     # Saturation to limit the maximal value that torques can have
-    torques = np.maximum(np.minimum(torques, torque_sat * np.ones((8,1))), -torque_sat * np.ones((8,1))) 
+    torques = np.maximum(np.minimum(torques, torque_sat * np.ones((8, 1))), -torque_sat * np.ones((8, 1)))
 
     return torques
