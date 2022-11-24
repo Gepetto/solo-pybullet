aceee332058df4d6a480ec865d4d19d3a93098ad
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-11-24 14:04:42 +0100

    format

--- solo_pybullet/controller.py
+++ solo_pybullet/controller.py
@@ -2,6 +2,8 @@
 
 # Other modules
 import numpy as np
+from numpy.linalg import pinv, inv
+
 # Pinocchio modules
 import pinocchio as pin  # Pinocchio library
 
@@ -13,11 +15,13 @@ from .PD import PD
 
 
 def c(q, qdot, dt):
-    # unactuated, [x, y, z] position of the base + [x, y, z, w] orientation of the base (stored as a quaternion)
+    # unactuated, [x, y, z] position of the base
+    # + [x, y, z, w] orientation of the base (stored as a quaternion)
     # qu = q[:7]
     # actuated, [q1, q2, ..., q8] angular position of the 8 motors
     qa = q[7:]
-    # [v_x, v_y, v_z] linear velocity of the base and [w_x, w_y, w_z] angular velocity of the base along x, y, z axes
+    # [v_x, v_y, v_z] linear velocity of the base
+    # and [w_x, w_y, w_z] angular velocity of the base along x, y, z axes
     # of the world
     # qu_dot = qdot[:6]
     qa_dot = qdot[6:]  # angular velocity of the 8 motors
@@ -32,7 +36,7 @@ def c(q, qdot, dt):
     ###############################################
 
     # Parameters for the PD controller
-    Kp = 8.
+    Kp = 8.0
     Kd = 0.06
     torque_sat = 3  # torque saturation in N.m
     torques_ref = np.zeros((8, 1))  # feedforward torques
@@ -40,13 +44,14 @@ def c(q, qdot, dt):
     # Call the PD controller
     torques = PD(qa_ref, qa_dot_ref, qa, qa_dot, dt, Kp, Kd, torque_sat, torques_ref)
 
-    # torques must be a numpy array of shape (8, 1) containing the torques applied to the 8 motors
+    # torques must be a numpy array of shape (8, 1)
+    # containing the torques applied to the 8 motors
     return torques
 
 
-########################
-## WALKING_CONTROLLER ##
-########################
+# #######################
+# # WALKING_CONTROLLER ##
+# #######################
 
 # Method : Inverse Kinematics
 
@@ -56,10 +61,12 @@ flag_q_ref = True
 
 
 def c_walking_IK(q, qdot, dt, solo, t_simu):
-    # unactuated, [x, y, z] position of the base + [x, y, z, w] orientation of the base (stored as a quaternion)
+    # unactuated, [x, y, z] position of the base
+    # + [x, y, z, w] orientation of the base (stored as a quaternion)
     # qu = q[:7]
     qa = q[7:]  # actuated, [q1, q2, ..., q8] angular position of the 8 motors
-    # [v_x, v_y, v_z] linear velocity of the base and [w_x, w_y, w_z] angular velocity of the base along x, y, z axes
+    # [v_x, v_y, v_z] linear velocity of the base
+    # and [w_x, w_y, w_z] angular velocity of the base along x, y, z axes
     # of the world
     # qu_dot = qdot[:6]
     qa_dot = qdot[6:]  # angular velocity of the 8 motors
@@ -67,10 +74,8 @@ def c_walking_IK(q, qdot, dt, solo, t_simu):
     qa_ref = np.zeros((8, 1))  # target angular positions for the motors
     qa_dot_ref = np.zeros((8, 1))  # target angular velocities for the motors
 
-    ###############################################
+    # ##############################################
     # Insert code here to set qa_ref and qa_dot_ref
-
-    from numpy.linalg import pinv
 
     global q_ref, flag_q_ref, T, dx, dz
 
@@ -79,7 +84,7 @@ def c_walking_IK(q, qdot, dt, solo, t_simu):
         flag_q_ref = False
 
     # Initialization of the variables
-    K = 100.  # Convergence gain
+    K = 100.0  # Convergence gain
     T = 0.2  # period of the foot trajectory
     xF0 = 0.19  # initial position of the front feet
     xH0 = -0.19  # initial position of the hind feet
@@ -101,7 +106,7 @@ def c_walking_IK(q, qdot, dt, solo, t_simu):
         if t >= T:
             t %= T
         x.append(x0 - dx * np.cos(2 * np.pi * t / T))
-        if t <= T / 2.:
+        if t <= T / 2.0:
             z.append(z0 + dz * np.sin(2 * np.pi * t / T))
         else:
             z.append(0)
@@ -136,7 +141,8 @@ def c_walking_IK(q, qdot, dt, solo, t_simu):
     oR_HR = solo.data.oMf[ID_HR].rotation
 
     # Getting the different Jacobians
-    fJ_FL3 = pin.frameJacobian(solo.model, solo.data, q_ref, ID_FL)[:3, -8:]  # Take only the translation terms
+    # Take only the translation terms
+    fJ_FL3 = pin.frameJacobian(solo.model, solo.data, q_ref, ID_FL)[:3, -8:]
     oJ_FL3 = oR_FL * fJ_FL3  # Transformation from local frame to world frame
     oJ_FLxz = oJ_FL3[0::2, -8:]  # Take the x and z components
 
@@ -167,13 +173,15 @@ def c_walking_IK(q, qdot, dt, solo, t_simu):
     qa_ref = q_ref[7:]
 
     # DONT FORGET TO RUN GEPETTO-GUI BEFORE RUNNING THIS PROGRAMM #
-    solo.display(q)  # display the robot in the viewer Gepetto-GUI given its configuration q
+    solo.display(
+        q
+    )  # display the robot in the viewer Gepetto-GUI given its configuration q
 
     # End of the control code
     ###############################################
 
     # Parameters for the PD controller
-    Kp = 8.
+    Kp = 8.0
     Kd = 0.2
     torque_sat = 3  # torque saturation in N.m
     torques_ref = np.zeros((8, 1))  # feedforward torques
@@ -181,7 +189,8 @@ def c_walking_IK(q, qdot, dt, solo, t_simu):
     # Call the PD controller
     torques = PD(qa_ref, qa_dot_ref, qa, qa_dot, dt, Kp, Kd, torque_sat, torques_ref)
 
-    # torques must be a numpy array of shape (8, 1) containing the torques applied to the 8 motors
+    # torques must be a numpy array of shape (8, 1)
+    # containing the torques applied to the 8 motors
     return torques
 
 
@@ -195,11 +204,13 @@ qa_dot2_ref = np.zeros((8, 1))
 
 def c_walking_ID(q, qdot, dt, solo, t_simu):
 
-    qu = q[:
-           7]  # unactuated, [x, y, z] position of the base + [x, y, z, w] orientation of the base (stored as a quaternion)
+    # unactuated, [x, y, z] position of the base
+    # + [x, y, z, w] orientation of the base (stored as a quaternion)
+    # qu = q[:7]
     qa = q[7:]  # actuated, [q1, q2, ..., q8] angular position of the 8 motors
-    qu_dot = qdot[:
-                  6]  # [v_x, v_y, v_z] linear velocity of the base and [w_x, w_y, w_z] angular velocity of the base along x, y, z axes of the world
+    # [v_x, v_y, v_z] linear velocity of the base
+    # and [w_x, w_y, w_z] angular velocity of the base along x, y, z axes of the world
+    # qu_dot = qdot[:6]
     qa_dot = qdot[6:]  # angular velocity of the 8 motors
 
     qa_ref = np.zeros((8, 1))  # target angular positions for the motors
@@ -208,8 +219,6 @@ def c_walking_ID(q, qdot, dt, solo, t_simu):
     ###############################################
     # Insert code here to set qa_ref and qa_dot_ref
 
-    from numpy.linalg import pinv, inv
-
     global qa_dot2_ref, q_dot2_ref, q_dot_ref, q_ref, flag_q_ref, T, dx, dz
 
     if flag_q_ref:
@@ -217,14 +226,14 @@ def c_walking_ID(q, qdot, dt, solo, t_simu):
         q_dot_ref = qdot
         flag_q_ref = False
 
-    #Initialization of the variables
-    K = 100.  #Convergence gain
-    T = 0.2  #period of the foot trajectory
-    xF0 = 0.19  #initial position of the front feet
-    xH0 = -0.19  #initial position of the hind feet
-    z0 = 0.0  #initial altitude of each foot
-    dx = 0.03  #displacement amplitude by x
-    dz = 0.06  #displacement amplitude by z
+    # Initialization of the variables
+    # K = 100.0  # Convergence gain
+    T = 0.2  # period of the foot trajectory
+    xF0 = 0.19  # initial position of the front feet
+    xH0 = -0.19  # initial position of the hind feet
+    z0 = 0.0  # initial altitude of each foot
+    dx = 0.03  # displacement amplitude by x
+    dz = 0.06  # displacement amplitude by z
 
     # Get the frame index of each foot
     ID_FL = solo.model.getFrameId("FL_FOOT")
@@ -233,14 +242,14 @@ def c_walking_ID(q, qdot, dt, solo, t_simu):
     ID_HR = solo.model.getFrameId("HR_FOOT")
 
     # function defining the feet's trajectory
-    def ftraj(t, x0, z0):  #arguments : time, initial position x and z
+    def ftraj(t, x0, z0):  # arguments : time, initial position x and z
         global T, dx, dz
         x = []
         z = []
         if t >= T:
             t %= T
         x.append(x0 - dx * np.cos(2 * np.pi * t / T))
-        if t <= T / 2.:
+        if t <= T / 2.0:
             z.append(z0 + dz * np.sin(2 * np.pi * t / T))
         else:
             z.append(0)
@@ -267,6 +276,7 @@ def c_walking_ID(q, qdot, dt, solo, t_simu):
     err_FR = xz_FR - xzdes_FR
     err_HL = xz_HL - xzdes_HL
     err_HR = xz_HR - xzdes_HR
+    print(err_FL, err_FR, err_HL, err_HR)
 
     # Computing the local Jacobian into the global frame
     oR_FL = solo.data.oMf[ID_FL].rotation
@@ -275,9 +285,10 @@ def c_walking_ID(q, qdot, dt, solo, t_simu):
     oR_HR = solo.data.oMf[ID_HR].rotation
 
     # Getting the different Jacobians
-    fJ_FL3 = pin.frameJacobian(solo.model, solo.data, q_ref, ID_FL)[:3, -8:]  #Take only the translation terms
-    oJ_FL3 = oR_FL * fJ_FL3  #Transformation from local frame to world frame
-    oJ_FLxz = oJ_FL3[0::2, -8:]  #Take the x and z components
+    # Take only the translation terms
+    fJ_FL3 = pin.frameJacobian(solo.model, solo.data, q_ref, ID_FL)[:3, -8:]
+    oJ_FL3 = oR_FL * fJ_FL3  # Transformation from local frame to world frame
+    oJ_FLxz = oJ_FL3[0::2, -8:]  # Take the x and z components
 
     fJ_FR3 = pin.frameJacobian(solo.model, solo.data, q_ref, ID_FR)[:3, -8:]
     oJ_FR3 = oR_FR * fJ_FR3
@@ -291,12 +302,16 @@ def c_walking_ID(q, qdot, dt, solo, t_simu):
     oJ_HR3 = oR_HR * fJ_HR3
     oJ_HRxz = oJ_HR3[0::2, -8:]
 
+    print(oJ_FLxz, oJ_FRxz, oJ_HLxz, oJ_HRxz)
+
     # Computing the mass matrix M and the dynamic drift b
-    M = pin.crba(solo.model, solo.data, q_ref)  #compute the mass matrix
-    b = pin.rnea(solo.model, solo.data, q_ref, q_dot_ref, q_dot2_ref)  #compute the dynamic drift
+    M = pin.crba(solo.model, solo.data, q_ref)  # compute the mass matrix
+    b = pin.rnea(
+        solo.model, solo.data, q_ref, q_dot_ref, q_dot2_ref
+    )  # compute the dynamic drift
 
     # Calculating reference torques
-    Kp_tau = 8.
+    Kp_tau = 8.0
     Kd_tau = 0.2
     torques_ref = -Kp_tau * (q_ref[7:] - q[7:]) - Kd_tau * qa_dot_ref
 
@@ -313,13 +328,15 @@ def c_walking_ID(q, qdot, dt, solo, t_simu):
     qa_ref = q_ref[7:]
 
     # DONT FORGET TO RUN GEPETTO-GUI BEFORE RUNNING THIS PROGRAMM #
-    solo.display(q)  # display the robot in the viewer Gepetto-GUI given its configuration q
+    solo.display(
+        q
+    )  # display the robot in the viewer Gepetto-GUI given its configuration q
 
     # End of the control code
     ###############################################
 
     # Parameters for the PD controller
-    Kp = 8.
+    Kp = 8.0
     Kd = 0.2
     torque_sat = 3  # torque saturation in N.m
     torques_ref = np.zeros((8, 1))  # feedforward torques
@@ -327,5 +344,6 @@ def c_walking_ID(q, qdot, dt, solo, t_simu):
     # Call the PD controller
     torques = PD(qa_ref, qa_dot_ref, qa, qa_dot, dt, Kp, Kd, torque_sat, torques_ref)
 
-    # torques must be a numpy array of shape (8, 1) containing the torques applied to the 8 motors
+    # torques must be a numpy array of shape (8, 1)
+    # containing the torques applied to the 8 motors
     return torques
--- solo_pybullet/initialization_simulation.py
+++ solo_pybullet/initialization_simulation.py
@@ -31,25 +31,32 @@ def configure_simulation(dt, enableGUI):
     # Load the robot for PyBullet
     robotStartPos = [0, 0, 0.35]
     robotStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
-    p.setAdditionalSearchPath("/opt/openrobots/share/example-robot-data/solo_description/robots")
+    p.setAdditionalSearchPath(
+        "/opt/openrobots/share/example-robot-data/solo_description/robots"
+    )
     robotId = p.loadURDF("solo.urdf", robotStartPos, robotStartOrientation)
 
     # Set time step of the simulation
     # dt = 0.001
     p.setTimeStep(dt)
-    # realTimeSimulation = True # If True then we will sleep in the main loop to have a frequency of 1/dt
+    # If True then we will sleep in the main loop to have a frequency of 1/dt
+    # realTimeSimulation = True
 
     # Disable default motor control for revolute joints
     revoluteJointIndices = [0, 1, 3, 4, 6, 7, 9, 10]
-    p.setJointMotorControlArray(robotId,
-                                jointIndices=revoluteJointIndices,
-                                controlMode=p.VELOCITY_CONTROL,
-                                targetVelocities=[0.0 for m in revoluteJointIndices],
-                                forces=[0.0 for m in revoluteJointIndices])
+    p.setJointMotorControlArray(
+        robotId,
+        jointIndices=revoluteJointIndices,
+        controlMode=p.VELOCITY_CONTROL,
+        targetVelocities=[0.0 for m in revoluteJointIndices],
+        forces=[0.0 for m in revoluteJointIndices],
+    )
 
     # Enable torque control for revolute joints
     jointTorques = [0.0 for m in revoluteJointIndices]
-    p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
+    p.setJointMotorControlArray(
+        robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques
+    )
 
     # Compute one step of simulation for initialization
     p.stepSimulation()
@@ -57,17 +64,34 @@ def configure_simulation(dt, enableGUI):
     return robotId, solo, revoluteJointIndices
 
 
-# Function to get the position/velocity of the base and the angular position/velocity of all joints
 def getPosVelJoints(robotId, revoluteJointIndices):
+    """get the position/velocity of the base and the angular position/velocity of all
+    joints."""
 
     jointStates = p.getJointStates(robotId, revoluteJointIndices)  # State of all joints
-    baseState = p.getBasePositionAndOrientation(robotId)  # Position of the free flying base
+    baseState = p.getBasePositionAndOrientation(
+        robotId
+    )  # Position of the free flying base
     baseVel = p.getBaseVelocity(robotId)  # Velocity of the free flying base
 
     # Reshaping data into q and qdot
-    q = np.vstack((np.array([baseState[0]]).transpose(), np.array([baseState[1]]).transpose(),
-                   np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).transpose()))
-    qdot = np.vstack((np.array([baseVel[0]]).transpose(), np.array([baseVel[1]]).transpose(),
-                      np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).transpose()))
+    q = np.vstack(
+        (
+            np.array([baseState[0]]).transpose(),
+            np.array([baseState[1]]).transpose(),
+            np.array(
+                [[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]
+            ).transpose(),
+        )
+    )
+    qdot = np.vstack(
+        (
+            np.array([baseVel[0]]).transpose(),
+            np.array([baseVel[1]]).transpose(),
+            np.array(
+                [[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]
+            ).transpose(),
+        )
+    )
 
     return q, qdot
